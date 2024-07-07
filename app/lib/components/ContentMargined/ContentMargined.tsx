e8fbc0ff64b23c1d4fc36109e7ce2176bf6bdd36
vaarvik <47792591+vaarvik@users.noreply.github.com>
2024-07-07 18:26:24 +0200

    Run formatter on all files

--- app/lib/components/ContentMargined/ContentMargined.tsx
+++ app/lib/components/ContentMargined/ContentMargined.tsx
@@ -2,30 +2,30 @@ import { ElementType, HTMLAttributes } from 'react';
 import styles from './ContentMargined.module.scss';
 
 interface Props extends Omit<HTMLAttributes<HTMLElement>, 'className'> {
-    as?: ElementType;
-    size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
-    ignoreX?: boolean;
-    ignoreY?: boolean;
+  as?: ElementType;
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+  ignoreX?: boolean;
+  ignoreY?: boolean;
 }
 
 export default function ContentMargined({
-    as: HTMLTag = 'div',
-    size = 'md',
-    ignoreX = false,
-    ignoreY = false,
-    children,
-    ...otherProps
+  as: HTMLTag = 'div',
+  size = 'md',
+  ignoreX = false,
+  ignoreY = false,
+  children,
+  ...otherProps
 }: Props) {
-    const classNames = [
-        styles['content-margined'],
-        size ? styles[`content-margined--${size}`] : '',
-        ignoreX ? styles['content-margined--ignore-x'] : '',
-        ignoreY ? styles['content-margined--ignore-y'] : '',
-    ].join(' ');
+  const classNames = [
+    styles['content-margined'],
+    size ? styles[`content-margined--${size}`] : '',
+    ignoreX ? styles['content-margined--ignore-x'] : '',
+    ignoreY ? styles['content-margined--ignore-y'] : '',
+  ].join(' ');
 
-    return (
-        <HTMLTag className={classNames} {...otherProps}>
-            {children}
-        </HTMLTag>
-    );
+  return (
+    <HTMLTag className={classNames} {...otherProps}>
+      {children}
+    </HTMLTag>
+  );
 }
--- app/lib/components/ContentPadded/ContentPadded.tsx
+++ app/lib/components/ContentPadded/ContentPadded.tsx
@@ -2,30 +2,30 @@ import { ElementType, HTMLAttributes } from 'react';
 import styles from './ContentPadded.module.scss';
 
 interface Props extends Omit<HTMLAttributes<HTMLElement>, 'className'> {
-    as?: ElementType;
-    size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
-    ignoreX?: boolean;
-    ignoreY?: boolean;
+  as?: ElementType;
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+  ignoreX?: boolean;
+  ignoreY?: boolean;
 }
 
 export default function ContentPadded({
-    as: HTMLTag = 'div',
-    size = 'md',
-    ignoreX = false,
-    ignoreY = false,
-    children,
-    ...otherProps
+  as: HTMLTag = 'div',
+  size = 'md',
+  ignoreX = false,
+  ignoreY = false,
+  children,
+  ...otherProps
 }: Props) {
-    const classNames = [
-        styles['content-padded'],
-        size ? styles[`content-padded--${size}`] : '',
-        ignoreX ? styles['content-padded--ignore-x'] : '',
-        ignoreY ? styles['content-padded--ignore-y'] : '',
-    ].join(' ');
+  const classNames = [
+    styles['content-padded'],
+    size ? styles[`content-padded--${size}`] : '',
+    ignoreX ? styles['content-padded--ignore-x'] : '',
+    ignoreY ? styles['content-padded--ignore-y'] : '',
+  ].join(' ');
 
-    return (
-        <HTMLTag className={classNames} {...otherProps}>
-            {children}
-        </HTMLTag>
-    );
+  return (
+    <HTMLTag className={classNames} {...otherProps}>
+      {children}
+    </HTMLTag>
+  );
 }
