aac1735f84194becd8cb1a807cc0b73418e657f2
vaarvik <47792591+vaarvik@users.noreply.github.com>
2024-07-17 23:45:50 +0200

    Remove no-unsafe-assignment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -32,7 +32,6 @@ const settings = [
       '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
       '@typescript-eslint/no-unused-expressions': 'warn',
       '@typescript-eslint/no-unsafe-member-access': 'error',
-      '@typescript-eslint/no-unsafe-assignment': 'error',
       '@typescript-eslint/no-unsafe-call': 'error',
       '@typescript-eslint/no-var-requires': 'warn',
       'max-depth': ['warn', 3],
